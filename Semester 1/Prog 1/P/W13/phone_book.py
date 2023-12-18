33785583cf1c3d02fd34e54d03ca9db6287099f8
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2023-12-18 20:36:21 +0100

    Prog

--- Semester 1/Prog 1/P/W13/phone_book.py
+++ Semester 1/Prog 1/P/W13/phone_book.py
@@ -13,11 +13,24 @@ Date: 18.12.2023
 
 
 def print_numbers(numbers):
+    """
+    Print the names and corresponding phone numbers in the phone book.
+    :param numbers: phonebook to print
+    :return: printed values, each tuple on one line
+    """
     for name, number in numbers.items():
         print(name, number)
 
 
 def add_number(numbers, name, number):
+    """
+    Add a new tuple (name and phone number) to the phone book.
+    Checks also if person already exists in phonebook.
+    :param numbers: phonebook to add
+    :param name: name of person
+    :param number: phone number of person
+    :return: updated phonebook with new tuple
+    """
     if name not in numbers:
         numbers[name] = number
         print(f"Success: {name} has been added")
@@ -26,6 +39,13 @@ def add_number(numbers, name, number):
 
 
 def lookup_number(numbers, name):
+    """
+    Lookup and return the phone number for a given name.
+    Checks also if person exists in phonebook.
+    :param numbers: phonebook to lookup
+    :param name: name of person
+    :return: number of person
+    """
     try:
         number = numbers[name]
         return number
@@ -35,6 +55,13 @@ def lookup_number(numbers, name):
 
 
 def remove_number(numbers, name):
+    """
+    Remove a name and its corresponding phone number from the phone book.
+    Checks also if person exists in phonebook.
+    :param numbers: phonebook
+    :param name: name you want to delete
+    :return: updated phonebook without removed tuple
+    """
     try:
         del numbers[name]
         print(f"Success: {name} has been removed")
@@ -43,6 +70,13 @@ def remove_number(numbers, name):
         return ""
 
 def load_numbers(numbers, filename):
+    """
+    Load phone numbers from a file and add them to the phone book.
+    Checks also if file exists in folder.
+    :param numbers: variable to store the imported tuples
+    :param filename: filepath of file you want to import
+    :return: updated variable numbers with new tuples
+    """
     try:
         with open(filename, "r") as f:
             for line in f:
@@ -55,6 +89,14 @@ def load_numbers(numbers, filename):
         return ""
 
 def save_numbers(numbers, filename):
+    """
+    Save phone numbers from the phone book to a file.
+    Gives also feedback wether save function was successfull
+    :param numbers: phonebook you want to store
+    :param filename: filename to store the phonebook
+    :return: no direct return,
+    result of function is a generated file with all tuples of phonebook
+    """
     with open(filename, "w") as f:
         for name, number in numbers.items():
             f.write(name + "," + number + "\n")
