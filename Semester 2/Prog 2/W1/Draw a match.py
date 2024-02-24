fab0a52b4bddc7f0c8cd340aab3c6a44eb47c4a2
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-02-24 14:43:29 +0100

    W1

---
+++ Semester 2/Prog 2/W1/Draw a match.py
@@ -0,0 +1,19 @@
+"""
+Prog 2
+W05
+Aufgabe 1.1 Draw a match
+
+Author: Jonas Bratschi (bratsjon)
+Date: 24.02.2024
+"""
+
+import random
+
+print("1: start game")
+print("2: Quit game")
+menu_number = int(input("Select your mode: "))
+start_number = random.randint(9, 20)
+while menu_number != 2:
+    match_horizontal = "=======0"
+    for line in range(start_number):
+        print(match_horizontal)
