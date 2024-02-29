8d5896245a4e56fa3abba79bf36e613b00f00a0c
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-02-29 20:20:17 +0100

    Prog1

---
+++ Semester 2/Prog 2/W2/P01_Classroom objects.py
@@ -0,0 +1,11 @@
+class document:
+    def __init__(self, name):
+        self.name = name
+
+class copies:
+    def __init__(self, document, copies):
+        self.document = document
+        self.copies = copies
+
+    def hand_over(self, document):
+        pass
