908ea7cca8a1bc7222717c442f830bd732207f1b
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-03-14 15:19:20 +0100

    Prog

--- Semester 2/Prog 2/W4/Dataclass.py
+++ Semester 2/Prog 2/W4/Dataclass.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
+import pydantic
 
-@dataclass
+@dataclass(frozen=True)
 class Bogen:
     name: str
     preis: int
     schaden: int = 100
-
 
 bogen1 = Bogen("Feuerbogen", 2500, 35)
 
