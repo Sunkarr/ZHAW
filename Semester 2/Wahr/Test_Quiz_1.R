64ddf3f664c2c4c898836755dca5aba67a04f5f9
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-03-22 11:32:54 +0100

    Wahr

--- Semester 2/Wahr/Test_Quiz_1.R
+++ Semester 2/Wahr/Test_Quiz_1.R
@@ -1,62 +1,48 @@
-n <- 100000
+# Würfeln gezinkt
 
-J <- matrix(NA, nrow = n, ncol = 5)
-for (i in 1:n) {
-  J[i,] <- sample(1:6, size = 5, replace = T)}
+nsim <- 100000
+w <- 0
+counter <- 0
+counterend <-0
+probs_1 <- c(3.5,3.5,3.5,3.5,41,45)/100
+for (i in 1:nsim) {
+  for (x in 1:3) {
+    Jen<- sum(sample(1:6, size = 4, replace = TRUE))
+    Sil<-sum(sample(1:6, size = 4,prob = probs_1, replace = TRUE))
+    if (Sil > Jen) {
+      counter = counter + 1
+    }
+    
+  }
+  if (counter == 3) {
+    counterend = counterend + 1
+    
+  }
+  counter = 0
+}
+counterend / nsim
 
-p <- c(87/4, 87/4, 87/4, 87/4, 44, 43)/100
-S <- matrix(NA, nrow = n, ncol = 5)
-for (i in 1:n) {
-  S[i,] <- sample(1:6, size = 5, replace = T, prob = p)}
 
-S <- as.data.frame(S)
-S$Sum <- S$V1 + S$V2 + S$V3 + S$V4 +S$V5
-
-J <- as.data.frame(J)
-J$Sum <- J$V1 + J$V2 + J$V3 + J$V4 +J$V5
-
-win_S <- S$Sum > J$Sum
-sum(win_S)
-
-
-
-
-
-set.seed(42) # Setzen des Zufallszahlengenerators für die Reproduzierbarkeit
-
-# Funktion zur Simulation des Ausfalls einer Windturbine an einem Tag
-turbine_ausfall <- function(wahrscheinlichkeit) {
-  if (runif(1) < wahrscheinlichkeit) {
-    return(TRUE)
-  } else {
-    return(FALSE)
+nsim <- 100000
+props <- c(0.0325, 0.0325, 0.0325, 0.0325, 0.44, 0.43)
+count <- 0
+for (i in 1:nsim) {
+  silvan_count <- 0
+  for (j in 1:3) {
+    silvan <- sample(1:6, size=5, replace = T, prob=props)
+    jennefer <- sample(1:6, size=5, replace=T)
+    if (sum(silvan) > sum(jennefer)) {
+      silvan_count <- silvan_count + 1
+    }
+  }
+  if (silvan_count == 3) {
+    count <- count + 1
   }
 }
 
-# Funktion zur Simulation der Anzahl von Turbinenausfällen in 27 Tagen
-simuliere_ausfälle <- function(n) {
-  ausfälle <- replicate(n, {
-    anzahl_ausfälle <- 0
-    for (i in 1:27) {
-      if (turbine_ausfall(0.08)) {
-        anzahl_ausfälle <- anzahl_ausfälle + 1
-      }
-    }
-    anzahl_ausfälle
-  })
-  return(ausfälle)
-}
+count/nsim
 
-# Durchführung der Simulation mit n = 100000
-sim_results <- simuliere_ausfälle(100000)
-
-# Berechnung des Anteils von Tagen mit 2 oder mehr Turbinenausfällen
-anteil <- mean(sim_results >= 2)
-
-# Ausgabe des Ergebnisses auf zwei Nachkommastellen
-print(round(anteil, 2))
-
-
+# Würfeln gezinkt 2
 
 n <- 10000
 p <- c(74/5, 74/5, 74/5, 74/5, 74/5, 26)/100
@@ -69,13 +55,17 @@ E <- sum(W)/n
 
 
 
+# Würfeln gezinkt 3
+
 nsim <- 100000
-count <- 0
-punkte <-0
+count = 0
+punkte = 0
 totpunkte <- 0
-probs <- c(0.148,0.148,0.148,0.148,0.148,0.26)
+probs <- c(rep(((100-24)/5), 5), 24)/100
+würfel <- c(seq(1:6))
+E <- sum(probs*würfel)
 for (i in 1:nsim) {
-  while (punkte <44) {
+  while (punkte < 44) {
     punkte = punkte + sample(1:6, size = 1, replace = TRUE,prob = probs)
     count = count + 1
   }
@@ -87,39 +77,10 @@ for (i in 1:nsim) {
 totpunkte / nsim
 
 
-
-# Simulationsparameter
-n_turbines <- 24
-n_days <- 26
-p_failure <- 0.08
+# Turbinen
+n_turbines <- 20
+n_days <- 27
 n_simulations <- 100000
-
-# Funktion zur Simulation eines Tages
-simulate_day <- function() {
-  failures <- sum(runif(n_turbines) < p_failure)
-  return(failures)
-}
-
-# Simulationsschleife
-count_multiple_failures <- 0
-for (i in 1:n_simulations) {
-  failures_per_day <- sapply(1:n_days, simulate_day)
-  if (sum(failures_per_day >= 2) > 0) {
-    count_multiple_failures <- count_multiple_failures + 1
-  }
-}
-
-# Berechnung der Wahrscheinlichkeit und Ausgabe des Ergebnisses
-probability <- count_multiple_failures / n_simulations
-probability <- round(probability, 2)
-print(paste("Wahrscheinlichkeit, dass 2 oder mehr Turbinen gleichzeitig ausfallen:", probability))
-
-
-
-# Simulationsparameter
-n_turbines <- 24
-n_days <- 26
-n_simulations <- 10000
 prob <- c(92, 8)/100
 
 M <- matrix(NA, nrow = n_simulations, ncol = n_days)
@@ -131,4 +92,15 @@ for (sim in 1:n_simulations){
   }
 }
 
-             
+count_elements_greater_than_equal_to_2 <- function(row) {
+  sum(row >= 2)
+}
+
+dat <- as.data.frame(M)
+
+dat$count <- apply(dat, 1, count_elements_greater_than_equal_to_2)
+
+mean(dat$count)
+
+
+
