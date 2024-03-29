7c50305b3713e1af2aa539d9ad5fcf7e5f5b20a2
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-03-29 14:27:47 +0100

    Wahr

--- Semester 2/Wahr/Wahr_P_5.R
+++ Semester 2/Wahr/Wahr_P_5.R
@@ -20,8 +20,39 @@ sum(dbinom(x = 1800:3720, size = 3720, prob = 0.5))
 
 # Aufgabe 3
 
-dbinom(x = 0:200, size = 600, prob = 0.005)
+sum(dbinom(2:4, 200, 0.015))
 
-sum(rbinom(200, 600, 0.005))
+sum(dbinom(2:4, 300, (1/90)))
+
+n <- 10000
+Filiale_A <- rbinom(n, 200, 0.015)
+Filiale_B <- rbinom(n, 300, (1/90))
+M <- as.data.frame(cbind(Filiale_A, Filiale_B))
+M$result[M$Filiale_A > M$Filiale_B] <- 0
+M$result[M$Filiale_A < M$Filiale_B] <- 1
+M$result[M$Filiale_A == M$Filiale_B] <- 0
+sum(M$result) / n
+
+
+# Aufgabe 4
+
+1 - pgeom(9, 1/7)
+plot(pgeom(0:9, 1/7))
+
+n <- 1000
+Leo <- rgeom(n, 10/70)
+smaller_than_20 <- function(vec) {
+  count <- 0
+  for (element in vec) {
+    if (element <= 20) {
+      count <- count + 1
+    }
+  }
+  return(count)
+}
+count <- smaller_than_20(Leo)
+prob_ei_mit_figur <- count / n
+prob_ei_mit_superheld <- prob_ei_mit_figur * (3/10)
+print(prob_ei_mit_superheld)
 
 
