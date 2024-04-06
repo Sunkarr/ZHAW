27dc21c826bfd848f05d6f41ec2e198359275fe8
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-04-06 18:16:05 +0200

    Prog Test

--- Semester 2/Prog 2/Test 1.py
+++ Semester 2/Prog 2/Test 1.py
@@ -1,14 +1,48 @@
-x = ["20", "Nov", "1994"]
-
-def parse_date(date):
-    monats_dict = {
-        "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4,
-        "May": 5, "Jun": 6, "Jul": 7, "Aug": 8,
-        "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12
-    }
-    result = f"{date[0]}.{monats_dict[date[1]]}.{date[2]}"
-    return result
+import requests
+from dataclasses import dataclass
+import os
+from datetime import datetime, timedelta
 
 
-y = parse_date(x)
-print(y)
+@dataclass(frozen=True)
+class Downloader:
+    URL = ("https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/"
+           "messwerte_tiefenbrunnen_seit2007-heute.csv")
+    FILENAME = "messwerte_tiefenbrunnen.csv"
+
+    def _new_download(self):
+        try:
+            response = requests.get(self.URL)
+            if response.status_code == 200:
+                file_content = response.content
+                with open(self.FILENAME, 'wb') as file:
+                    file.write(file_content)
+                print(f"'{self.FILENAME}' downloaded successfully!")
+            else:
+                print(f"Failed to download file from '{self.URL}'. Status code: {response.status_code}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def _check_age(self, filepath=FILENAME):
+        last_modified_datetime = datetime.fromtimestamp(os.path.getmtime(filepath))
+        current_time = datetime.now()
+        age = current_time - last_modified_datetime
+        return age
+
+    def download(self):
+        try:
+            age = self._check_age()
+            TEN_MINUTES = timedelta(minutes=10)
+
+            if age > TEN_MINUTES:
+                self._new_download()
+                print(f"'{self.FILENAME}' is now up to date")
+            else:
+                print(f"'{self.FILENAME}' is already up to date")
+        except FileNotFoundError:
+            self._new_download()
+            print(f"'{self.FILENAME}' is now stored")
+
+if __name__ == "__main__":
+    downloader = Downloader()
+    downloader.download()
