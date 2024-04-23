7b7bbeb950f695030adb6df52cc642e5012ae83a
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-04-23 22:03:31 +0200

    Wahr

--- Semester 2/Wahr/Wahr_P_9.R
+++ Semester 2/Wahr/Wahr_P_9.R
@@ -12,5 +12,34 @@ hist(rexp(n, 0.34) + rexp(n, 0.34) + rexp(n, 0.34), freq=F)
 lines(0:40, dgamma(0:40, shape = 3, rate = 0.34), col = "blue", lwd = 2)
 
 
+# Aufgabe 2
+
+n <- 100000
+kunden_wetter_schlecht <- rpois(n*0.4, 10)
+kunden_wetter_gut <- rpois(n*0.6, 30)
+kunden_total <- c(kunden_wetter_gut, kunden_wetter_schlecht)
+hist(kunden_total, freq = F)
+abline(v = mean(kunden_total), col = "red", lwd = 2)
+mean(kunden_total)
+
+q <- 0.1
+fixkosten_pro_tag <- -1000
+q_zusatzkosten_pro_kunde <- -qunif(1-q, min = 20, max = 30)
+q_einnahmen_pro_kunde <- 60 + qexp(q, 1/30)
+umsatz_pro_tag <- numeric(n)
+for (i in 1:n){
+  umsatz_pro_tag[i] <- fixkosten_pro_tag + kunden_total[i]*q_zusatzkosten_pro_kunde + kunden_total[i]*q_einnahmen_pro_kunde
+}
+umsatz_pro_tag_immer_offen <- mean(umsatz_pro_tag)
 
 
+umsatz_pro_tag <- numeric(n)
+for (i in 1:length(kunden_wetter_gut)){
+  umsatz_pro_tag[i] <- fixkosten_pro_tag + kunden_wetter_gut[i]*q_zusatzkosten_pro_kunde + kunden_wetter_gut[i]*q_einnahmen_pro_kunde
+}
+umsatz_pro_tag[umsatz_pro_tag == 0] <- -200
+umsatz_pro_tag_nur_schön_wetter_offen <- mean(umsatz_pro_tag)
+
+
+
+                                      
