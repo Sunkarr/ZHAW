56f734a0bfcbcf71f045056d6018da9fb6ae4750
Jonas Bratschi <bratsjon@students.zhaw.ch>
2025-02-19 21:54:16 +0100

    pm2

--- Semester 4/PM 2/P1.R
+++ Semester 4/PM 2/P1.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(dplyr)
+library(ggplot2)
 
 # Aufgabe 1
 
@@ -73,5 +74,58 @@ starwars |>
 starwars |>
   mutate_if(is.numeric, scale)
 
+
+# Aufgabe 3
+
+dataPath <- "/Users/jonas/Library/CloudStorage/OneDrive-ZHAW/Sem 4/PM 2/data/Uebung01/exercises_fish.csv"
+fish_df <- read_csv(file = dataPath,
+                    col_types = cols(
+                      salmon_id = col_integer(),
+                      `common_name-size` = col_factor(),
+                      variable = col_factor(),
+                      value = col_double()))
+fish_df |>
+  separate(`common_name-size`, sep = " - ", into=c("name", "age"), remove=TRUE) |>
+  pivot_wider(names_from = variable, values_from = value) -> fish_df
+
+fish_df |>
+  filter(name == "Steelhead") |>
+  select(age, IGF1_ng_ml)
+
+fish_df |>
+  filter(str_starts(name,'S')) |>
+  select( -IGF1_ng_ml) |>
+  arrange(name, desc(length_mm))
+
+fish_df |>
+  group_by(age) |>
+  filter(rank(length_mm) <= 5) |>
+  summarise(mean_length = mean(length_mm),
+            var_length = var(length_mm))
+
+fish_df |>
+  filter(age == "yearling") |>
+  group_by(name) |>
+  mutate(stan_length = scale(length_mm)) |>
+  ggplot(aes(x = name, y = stan_length)) +
+  geom_boxplot()
+
+
+# Aufgabe 4
+
+mtcars <- tibble::as_tibble(mtcars)
+
+mtcars |>
+  filter(hp < 100) |>
+  summarise(count = n())
+
+mtcars |>
+  select(-c(gear, carb)) -> dat
+
+dat |>
+  arrange(desc(cyl), desc(hp)) -> dat
+
+dat |>
+  rename(power = hp) -> dat
 
 
