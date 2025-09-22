b44a76b6a0e8c2d4d3695996fabd85bda3dced0a
Jonas Bratschi <bratsjon@students.zhaw.ch>
2025-09-22 21:04:44 +0200

    GStat

--- Semester 5/GStat/AB01.r
+++ Semester 5/GStat/AB01.r
@@ -28,3 +28,43 @@ pexp(10, rate = 2/5) - pexp(2, rate = 2/5)
 
 funktion <- function(x) dexp(x = x, rate = 2/5)
 integrate(funktion, lower = 2, upper = 10)
+
+
+# Aufgabe 2: Erwartungswert
+
+# Wie lange müssen Sie im Mittel auf einen Anruf warten (gleiche Bedingungen
+# wie in der Aufgabe 1)? Ermitteln Sie den Erwartungswert durch:
+
+# a) Simulation (10000 Realisationen)
+data <- rexp(10000, rate = 2/5)
+mean(data)
+
+# b) numerische Integration. Hinweis: ∞ kann in integrate() als Inf geschrieben werden.
+funktion <- function(x) x * dexp(x = x, rate = 2/5)
+integrate(funktion, lower = 0, upper = Inf)
+
+
+# Aufgabe 3: Transformationen
+
+# Aufgabe a)
+pnorm(25, mean = 20, sd = 3, lower.tail = F)
+
+# b) Ein Team amerikanischer Kollegen möchte die Ergebnisse reproduzieren,
+# aber sie ziehen es vor, die Berechnungen in Grad Fahrenheit durchzuführen.
+# Wie ist die Verteilung der Temperatur in Grad Fahrenheit?
+
+# Umrechnung von Celsius in Fahrenheit: F = C * 9/5 + 32
+f_mean <- 20 * 9/5 + 32
+f_sd <- 3 * 9/5
+
+# c) Wiederholen Sie die Berechnung in a) in Grad Fahrenheit.
+pnorm(25 * 9/5 + 32, mean = f_mean, sd = f_sd, lower.tail = F)
+
+
+# Aufgabe 4: Meinungsumfrage
+
+# Aufgabe a)
+pbinom(100, 200, 0.45, lower.tail = F)
+
+# Aufgabe b)
+pbinom(108, 200, 0.45, lower.tail = F)
